f66052779e31910415392bd30a0566802b8abf46
Konashuk <130701560+Konashuk@users.noreply.github.com>
2023-09-14 14:46:06 +0000

    Deploying to gh-pages from @ Konashuk/goit-js-hw-09@99e84e2cadf005fd6c48871c29adb6c87973061b 🚀

---
+++ 01-color-switcher.818503c5.js
@@ -0,0 +1,2 @@
+!function(){var t,e=document.querySelector("[data-start]"),a=document.querySelector("[data-stop]");e.addEventListener("click",(function(n){e.disabled=!0,a.disabled=!1,t=setInterval((function(){var t="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0));document.body.style.backgroundColor=t}),1e3),console.log(e)})),a.addEventListener("click",(function(n){e.disabled=!1,a.disabled=!0,clearInterval(t)}))}();
+//# sourceMappingURL=01-color-switcher.818503c5.js.map
