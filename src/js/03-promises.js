bede7d363b8042e00b2fac8b00c80cb1b7d4065e
Konashuk <konan4ukv@gmail.com>
2023-09-20 19:32:55 +0300

    task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,47 @@
+// // all modules
+import Notiflix from 'notiflix';
+
+import 'notiflix/dist/notiflix-3.2.6.min.css';
+
+const form = document.querySelector('.form');
+const delayInput = form.querySelector('[name="delay"]');
+const stepInput = form.querySelector('[name="step"]');
+const amountInput = form.querySelector('[name="amount"]');
+
+form.addEventListener('submit', chekInput);
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+function chekInput(event) {
+  event.preventDefault();
+
+  const delay = Number(delayInput.value);
+  const step = Number(stepInput.value);
+  const amount = Number(amountInput.value);
+
+  for (let i = 1; i <= amount; i++) {
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
   }
 }
