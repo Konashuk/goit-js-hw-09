42f047e240e08ce388340a1bd8ae9608ff3125e2
Konashuk <konan4ukv@gmail.com>
2023-09-20 16:35:30 +0300

    task 2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -33,17 +33,18 @@ flatpickr('#datetime-picker', options);
 function startTimer(ev) {
   const timer = setInterval(() => {
     const curentTime = new Date();
-    const timeDifference = selectedDate - curentTime;
+    const ms = selectedDate - curentTime;
 
-    if (timeDifference <= 0) {
+    if (ms <= 0) {
       clearInterval(timer); // Зупинити таймер, якщо час вийшов
       return;
     }
 
-    const seconds = curentTime.getSeconds();
-    const minutes = curentTime.getMinutes();
-    const hours = curentTime.getHours();
-    const days = curentTime.getDate();
+    // const seconds = curentTime.getSeconds();
+    // const minutes = curentTime.getMinutes();
+    // const hours = curentTime.getHours();
+    // const days = curentTime.getDate();
+    const { days, hours, minutes, seconds } = convertMs(ms);
 
     daysEl.textContent = days;
     hoursEl.textContent = hours;
@@ -51,3 +52,26 @@ function startTimer(ev) {
     seconsdEl.textContent = seconds;
   }, 1000);
 }
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
+console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
+console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
